68aadf5aa5582ab30cb80d8b4bf6375628c97095
PPP-K <priyanshugangwar00@gmail.com>
2024-10-31 14:54:05 +0530

    Your commit message

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100, min_length=3,
-                               widget=forms.TextInput(attrs={'class': 'form'}))
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='Email', max_length=200, min_length=5,
-                             widget=forms.EmailInput(attrs={'class': 'form'}))
+                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Password', max_length=50, min_length=5,
-                                widget=forms.PasswordInput(attrs={'class': 'form'}))
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Confirm Password',
                                 max_length=50, min_length=5,
-                                widget=forms.PasswordInput(attrs={'class': 'form'}))
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
